f123322aa78eb7e2eb2d19cade88bbcf3284593c
SakshamGarg1 <86765712+SakshamGarg1@users.noreply.github.com>
2021-11-19 02:01:37 +0530

    Update index.js

--- block-BJaabg/code/index.js
+++ block-BJaabg/code/index.js
@@ -2,6 +2,7 @@
 🎖 Write a program to calculate the total price of your phone purchase. With these conditions
  * [ ] You will keep purchasing phones (hint: loop!) until you run out of bank balance.
  * [ ] You'll also buy accessories for each phone as long as your purchase amount is below your spending threshold.
+ 
  * [ ] After you've calculated your purchase amount, add in the tax, then print out the calculated purchase amount, properly formatted like ($334.76).
  * [ ] Finally, check the amount against your bank account balance to see if you can afford it or not.
 */
