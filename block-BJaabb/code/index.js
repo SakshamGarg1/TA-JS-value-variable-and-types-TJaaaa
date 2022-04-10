d8f8312c43bd95554c259b0b789d0e91b19388d0
SakshamGarg1 <86765712+SakshamGarg1@users.noreply.github.com>
2022-04-11 02:01:32 +0530

    Update index.js

--- block-BJaabb/code/index.js
+++ block-BJaabb/code/index.js
@@ -5,25 +5,64 @@
 3. Declare variable `age` and `isLearning` with value `22` and `true` in the same line. hint: declaring multiplemvariable in one line.
 4. Now change the value of `user` to `John` and log the value of both `user` and `userName`.
 */
+```js
+let user = prompt("Enter Your Name");
+alert user;
+let userName = user;
+console.log(userName);
+let age =23 , isLearning = true;
+user ="john"
+console.log(user,userName)
+```
+
+
 
 /* 2.
-Use `prompt` to take two numbers from user and store it in variable `numA` and `numB`. Create a third variable called `sum`, using `+` operator add numA and numB , store it in varible `sum` and using alert display `sum` in browser.
+Use `prompt` to take two numbers from user and store it in variable `numA` and `numB`.
+Create a third variable called `sum`, using `+` operator add numA and numB , store it in varible `sum` and using alert display `sum` in browser.
 */
+let num1 = prompt("Enter 1st number")
+let num2 = prompt("Enter 2nd number")
 
+let sum = num1 + num2 ;
+alert(sum);
 // Loops
 
 // 3. Complete the following code to make the output be 0 2 4 6 8 10?
 
 
-for (let j = 0; j <= 10; ____) console.log(j);
+for (let j = 0; j <= 10; j+=2){ console.log(j)};
 
 // 4. Print the odd numbers from 9 to 1 (both inclusive) using a for loop.
-
+for (let j = 0; j <= 9; j--){
+  if(j%2 !==0){
+  console.log(j)
+  }
+};
 // 5. Concat all the numbers from 5 to 0 (both inclusive) in descending order using a while loop. (543210)
+
+let finalString ="";
+let initialValue = "5";
+
+while(initialValue >=0){
+  finalString += initialValue;
+  initialValue --;
+}
+console.log(finalString)
 
 // 6. Concat all the even numbers from 10 to 0 (both inclusive) in descending order using a while loop. (1086420)
 
-// Comparisoin
+let finalString ="";
+let initialValue = "10";
+
+while(initialValue >=0){
+  if(initialValue %2 === 0;){
+  finalString += initialValue;
+ }
+  initialValue --;
+  
+}
+console.log(finalString)
 
 /* 7. Take two value using prompt and store them in variables `num1` and `num2`. Check whether they are equal or not.
 
